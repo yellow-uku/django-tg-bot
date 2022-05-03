1237b229db0581b39f83e687fd137f3e78c24599
yellow-uku <alina203451@gmail.com>
2022-05-03 19:43:29 +0300

    retrieve data about sender of forwarded message

--- tgbot/handlers/forward_messages/handlers.py
+++ tgbot/handlers/forward_messages/handlers.py
@@ -4,7 +4,7 @@ from telegram.ext import CallbackContext
 
 # from tgbot.handlers.location.static_text import share_location, thanks_for_location
 # from tgbot.handlers.location.keyboards import send_location_keyboard
-from tgbot.models import User
+# from tgbot.models import User
 
-def reply_to_forward_message(update: Update, context: CallbackContext) -> None:
-	update.message.reply_text(text='Recieve forwarded message!')
+def reply_to_forward_message(update: Update, context) -> None:
+	update.message.reply_text(text=f'Recieve forwarded message! Here is user data:{update.message.forward_from}')
